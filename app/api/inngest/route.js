49a68c53d4b1b8719953cf89117f0e59506d06ba
BhavyaProjects07 <laptopuse01824x@gmail.com>
2026-01-04 15:51:09 +0530

    small change

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,13 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+export const runtime = "nodejs";
 
-// Create an API that serves zero functions
+import { serve } from "inngest/next";
+import { inngest } from "@/inngest/client";
+import {
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "@/inngest/functions";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
