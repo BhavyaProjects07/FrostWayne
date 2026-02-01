7b1a93a8b11ea42e16be72d7dbce187b007e05ea
BhavyaProjects07 <laptopuse01824x@gmail.com>
2026-02-01 13:37:58 +0530

    fixed some errors

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,33 @@
+'use client'
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
+
+if (typeof window !== "undefined") {
+  window.onerror = function (msg, url, line, col, error) {
+    document.body.innerHTML = `
+      <pre style="
+        padding:16px;
+        background:#0f172a;
+        color:#ff4d4f;
+        white-space:pre-wrap;
+        font-size:14px;
+      ">
+ERROR:
+${msg}
+
+FILE:
+${url}:${line}:${col}
+
+STACK:
+${error?.stack}
+      </pre>
+    `
+  }
+}
 
 const outfit = Outfit({
   subsets: ["latin"],
